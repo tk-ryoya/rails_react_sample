6e40e7296af62aecb196681f97fba6c35a30515d
takeda_ryoya <t.ryooya@gmail.com>
2023-01-14 18:48:54 +0900

    Add: eventsコントローラを追加

---
+++ app/controllers/api/events_controller.rb
@@ -0,0 +1,48 @@
+class Api::EventsController < ApplicationController
+  before_action :set_event, only: %i[show update destory]
+
+  def index
+    @events = Event.all
+    render json: @events
+  end
+
+  def show
+    render json: @event
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      render json: @event, status: :created
+    else
+      render json: @event.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @event.update(event_params)
+      render json: @event, status: :ok
+    else
+      render json: @event.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @event.destroy
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:id,
+                                  :event_type,
+                                  :event_date,
+                                  :title,
+                                  :speaker,
+                                  :host,
+                                  :published,
+                                  :created_at,
+                                  :update_at)
+  end
+end
