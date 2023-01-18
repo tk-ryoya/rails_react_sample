c757302010fd6bab89045c04b17b417f57133364
takeda_ryoya <t.ryooya@gmail.com>
2023-01-18 12:12:48 +0900

    Add: イベント削除機能を追加

--- app/javascript/components/Event.js
+++ app/javascript/components/Event.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useParams } from 'react-router-dom';
 
-const Event = ({ events }) => {
+const Event = ({ events, onDelete }) => {
   const { id } = useParams();
   const event = events.find((e) => e.id === Number(id));
 
@@ -13,6 +13,13 @@ const Event = ({ events }) => {
           {event.event_date}
           {' - '}
           {event.event_type}
+        <button
+          className="delete"
+          type="button"
+          onClick={() => onDelete(event.id)}
+        >
+          Delete
+        </button>
         </h2>
         <ul>
           <li>
@@ -51,6 +58,7 @@ Event.propTypes = {
       published: PropTypes.bool.isRequired,
     })
   ).isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
 
 export default Event;
